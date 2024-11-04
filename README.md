f982e00260626aad946974b928d959c0ea007f5b
DarkMatter-D <dudeIgiveupL@protonmail.com>
2024-11-04 14:04:49 -0500

    Fixed some bugs, added stuff to the readme, always improving

--- README.md
+++ README.md
@@ -17,6 +17,9 @@ Open up a terminal and run
 ```
 python3 main_app.py
 ```
+all the files will be stored in the output folder, if you want to change this change lines 36 and 33 to specify you own path
+
+### This code works on both windows and linux without errors(so far)
 ## Executable
 
 if you want an executable for windows for ease you can just use the integrated python module that was already installed in requirements.txt 
@@ -24,3 +27,10 @@ if you want an executable for windows for ease you can just use the integrated p
 ```
 pyinstaller --onefile main_app.py --icon=icons/youtube.ico
 ```
+
+## Issues
+
+### If you have any problem with the program please dont hessitate to report it, be sure to leave a quick issue i will try to fix it ASAP
+
+## Screenshots
+
--- main_app.py
+++ main_app.py
@@ -11,28 +11,29 @@ main_app = customtkinter.CTk()
 main_app.geometry("400x300")
 main_app.title("Youtube Downloader")
 main_app.resizable(0,0)
-
+main_app.iconbitmap("icons/youtube.ico")
 
 #a bunch of labels
-text_box = customtkinter.CTkLabel(master=main_app,text="Enter Video URL:",font=('Fira Code', 15,'bold'))
+text_box = customtkinter.CTkLabel(main_app,text="Enter Video URL:",font=('Fira Code', 15,'bold'))
 text_box.place(x=70,y=30)
-error_label = customtkinter.CTkLabel(master=main_app, text="Invalid Url!",text_color="red",font=('Fira Code', 20, 'bold'),anchor="center")
-downloading_label = customtkinter.CTkLabel(master=main_app,text="Downloaded!",text_color="white",font=('Fira Code', 20, 'bold'),anchor="center")
+error_label = customtkinter.CTkLabel(main_app, text="Invalid Url!",text_color="red",font=('Fira Code', 20, 'bold'),anchor="center")
+downloading_label = customtkinter.CTkLabel(main_app,text="Downloaded!",text_color="white",font=('Fira Code', 20, 'bold'),anchor="center")
 
 
 #the main download function
 def main_download(url,mode):
     try:
-        video = YouTube(url,"ANDROID")
+        video = YouTube(url)
         error_label.destroy()
-        downloading_label.place(relx=0.5, rely=0.5,x=-80,y=50)
+        downloading_label.place(relx=0.5, rely=0.5,x=-60,y=50)
     except RegexMatchError:
-        error_label.place(relx=0.5, rely=0.5,x=-80,y=50)
+        error_label.place(relx=0.5, rely=0.5,x=-60,y=50)
     #mode checks "video" or "audio"
     if mode == "video":
-        video.streams.first().download()
+        video.streams.first().download(output_path="output")
     elif mode == "audio":
         video.streams.filter(only_audio=True)
+        video.streams.first().download(output_path="output",filename=video.title+".mp3")
 #select all event for ctrl+a
 def select_all(event=None):
     # select text
@@ -46,9 +47,9 @@ url_input_box.place(x=70,y=90)
 
 #main audio and video buttons
 audio_button = customtkinter.CTkButton(master=main_app,width=30,height=35,text="Download Audio",command=lambda : main_download(url_input_box.get(),"audio"),font=('Fira Code', 17))
-audio_button.place(x=30,y=150)
+audio_button.place(x=50,y=150)
 video_button = customtkinter.CTkButton(master=main_app,width=30,height=35,text="Download Video",command=lambda : main_download(url_input_box.get(),"video"),font=('Fira Code', 17))
-video_button.place(x=195,y=150)
+video_button.place(x=210,y=150)
 
 #text placeholder for the history
 
